7318018751ae96a0fbf7566cb331ba30b1d305b8
Phillip Puckett <phillippuckett88@gmail.com>
2015-10-28 21:17:01 -0600

    Revision

--- FriendsAngular/app.js
+++ FriendsAngular/app.js
@@ -1,1 +1,19 @@
 var app = angular.module('friendsList', []);
+
+
+/*
+Angular API Reference
+docs.angularjs.org
+
+What was covered in lecture:
+ng-app
+ng-controller
+$scope
+ng-model
+{{ }}
+ng-repeat
+filter
+ng-show and ng-hide
+ng-if
+ng-click
+*/
--- MiniProjectAngular/js/mainCtrl.js
+++ MiniProjectAngular/js/mainCtrl.js
@@ -1,5 +1,3 @@
-var app = angular.module('friendsList');
-
 app.controller('mainCtrl', function($scope) {
    $scope.name = 'Phillip';
 $scope.friends = ['Miles', 'Luke', 'TJ']; 
